6c936eca9dc47ef8a65c75a64c20cccf2f80c6d3
SuryaChakkilala <2000030167@kluniversity.in>
2022-02-26 23:51:13 +0530

    commit

--- zap/settings.py
+++ zap/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]         # new
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))            # new
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
