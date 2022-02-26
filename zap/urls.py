142297af588e5e8beb577dcc92ae6e29bf0de4e7
SuryaChakkilala <2000030167@kluniversity.in>
2022-02-26 23:06:07 +0530

    commit

--- zap/urls.py
+++ zap/urls.py
@@ -17,7 +17,8 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-from django.views.static import serve
+from django.urls import re_path
+from django.contrib.staticfiles import views
 
 urlpatterns = [
     path('', include('store.urls')),
@@ -26,5 +27,7 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if not settings.DEBUG: 
-    urlpatterns += [ url(r'^uploads/(?P.)$', serve,{'document_root': settings.MEDIA_ROOT}), url(r'^static/(?P.)$', serve,{'document_root': settings.STATIC_ROOT}), ]
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', views.serve),
+    ]
