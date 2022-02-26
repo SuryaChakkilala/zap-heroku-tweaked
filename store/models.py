65213e2838951e24e1b462e2a8996427548e6b49
SuryaChakkilala <2000030167@kluniversity.in>
2022-02-27 00:08:03 +0530

    added rename file

--- store/models.py
+++ store/models.py
@@ -3,10 +3,19 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+def path_and_rename(instance, filename):
+    upload_to = 'images'
+    ext = filename.split('.')[-1]
+    # get filename
+    if instance.pk:
+        filename = '{}.{}'.format(instance.pk, ext)
+    # return the whole path to the file
+    return os.path.join(upload_to, filename)
+
 class Product(models.Model):
     name = models.CharField(max_length=200, unique=True)
     price = models.FloatField()
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(upload_to=path_and_rename, null=True, blank=True)
     description = models.TextField(default='desc')
 
     def __str__(self):
